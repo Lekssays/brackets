069590eef42196ffa401d9f90c39caf369a8447f
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2016-12-25 19:27:20 +0000

    :construction: Implementig the run

--- desktop/README.md
+++ desktop/README.md
@@ -1,37 +1,27 @@
-# Submit
+# Submit - Desktop
 
 Programming Contests Judging Tool  
 
 # Notes  
 
-Electron Documentation: https://github.com/electron/electron/tree/master/docs/api
-  
-The app uses web-terminal to access the terminal.  
-To install use:
-  sudo npm install web-terminal -g  
-  
-To open web-terminal in a port use:  
-  web-terminal --port [portNumber]  
-  
-To manipulate the terminal check the docs: https://github.com/rabchev/web-terminal 
-
+Electron Documentation: https://github.com/electron/electron/tree/master/docs/api 
 
 # TODO  
 ## General
 - Get the data in https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js locally and change the link address(~ line 31)  
 - Implement run  
-- Implement submit
+- Implement submit  
+- Implement sessions  
 - Improve UI  
+- Configure the coding style: https://github.com/electron/electron/blob/master/docs/development/coding-style.md
 
 ## Webviews  
-- Change http://127.0.0.1:8088/terminal/ to the corresponding address in the server
 - Change webview link to the scoreboard address
 
 ## Editor
-- Make it possible to change languages in the editor
-- Get data from editor (editor.getValue(); // or session.getValue)  
+- Add Possibility to change languages in the editor  
 
 ## Dropdown Boxes
-- separate the Dropdown selectors  
+- separate the Dropdown selection  
  
 
--- desktop/functions.js
+++ desktop/functions.js
@@ -1,21 +1,45 @@
-function saveFile(filename, data) {
-    var file = new File(__dirname + '/' + filename);
+/* TODO: Fix the coding style to follow:
+ https://github.com/electron/electron/blob/master/docs/development/coding-style.md
+*/
+
+function getDataFromEditor () {
+    return ace.edit("editor").getValue();
+}
+
+function saveFile (filename, data) {
+    // TODO: Change __dirname to a more appropriate directory
+    let file = new File(__dirname + '/' + filename);
     file.open("w");
     file.writeln(data);
     file.close();
 }
 
-function compileC(filename) {
-    var exec = require('child_process').exec, child;
-    var ret = '';
+function getCompilingCommand (language) {
+    // TODO: Get the value from the json file
+    // TODO: Form an appropriate command to return
+    let obj = $.parseJSON();
 
-child = exec('ls', ret =
-    function (error, stdout, stderr) {
-        //document.append('stdout: ' + stdout);
-        ret += stdout;
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
-    return ret;
+}
+
+function Compile (filename, language) {
+    // TODO: Complete the command
+    // TODO: Execute the command in another directory
+    let command = getCompilingCommand(language);
+    require('child_process').exec('ls',
+        function (error, stdout, stderr) {
+            //document.append('stdout: ' + stdout);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+            let node = document.createElement("p");
+            "use strict";
+            for (i = 0; i < stdout.length; i++) {
+                if (stdout[i] == '\n') {
+                    document.getElementById("comp").appendChild(document.createElement('br'));
+                    console.log("hi\n");
+                } else {
+                    document.getElementById("comp").appendChild(document.createTextNode(stdout[i]));
+                }
+            }
+        });
 }
