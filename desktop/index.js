9f24a79f654fe9557684016a8fc4bc3fede69f84
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2016-12-25 20:12:14 +0000

    :construction: Implementing the run

--- desktop/index.js
+++ desktop/index.js
@@ -1,14 +1,11 @@
 const {app, BrowserWindow} = require('electron')
 app.commandLine.appendSwitch('')
 
- //var http        = require("http"),
-   //     terminal    = require("web-terminal");
-
 app.on('ready', function() {
     let mainWindow = new BrowserWindow({
         minWidth: 1250,
         minHeight: 800
     })
     mainWindow.loadURL('file://' + __dirname + '/index.html')
-    mainWindow.openDevTools()
+    //mainWindow.openDevTools()
 })
